c367b4ab75d9504cca6a1c6b345a3c4c070eb88a
singedli <634814660@qq.com>
2019-12-03 14:32:46 +0800

    first commit

---
+++ src/api/cache.js
@@ -0,0 +1,10 @@
+import request from '@/utils/request'
+
+export function getList(params) {
+  return request({
+    baseURL: 'http://localhost:9000/gateway',
+    url: '/cache/config/all',
+    method: 'get',
+    params
+  })
+}
---
+++ src/views/cache/index.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="app-container">
+
+    <el-form :inline="true" :model="listQuery" class="demo-form-inline">
+      <el-form-item label="名称:">
+        <el-input v-model="listQuery.name" placeholder="名称" />
+      </el-form-item>
+
+      <el-form-item label="URL:">
+        <el-input v-model="listQuery.url" placeholder="URL" />
+      </el-form-item>
+
+      <el-form-item label="后台接口URL:">
+        <el-input v-model="listQuery.backonUrl" placeholder="后台接口URL" />
+      </el-form-item>
+
+      <el-form-item label="状态:">
+        <el-input v-model="listQuery.status" placeholder="状态" />
+      </el-form-item>
+      <!-- <el-form-item label="活动区域">
+        <el-select v-model="formInline.region" placeholder="活动区域">
+          <el-option label="区域一" value="shanghai" />
+          <el-option label="区域二" value="beijing" />
+        </el-select>
+      </el-form-item> -->
+      <el-form-item>
+        <el-button type="primary">查询</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table
+      :key="tableKey"
+      v-loading="listLoading"
+      :data="list"
+      border
+      fit
+      highlight-current-row
+      style="width: 100%;"
+      @sort-change="sortChange"
+    >
+      <el-table-column label="ID" prop="id" sortable="custom" align="center" width="80" :class-name="getSortClass('id')">
+        <template slot-scope="{row}">
+          <span>{{ row.id }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="名字" width="150px" align="center" prop="name" />
+      <el-table-column label="URL" width="150px" align="center" prop="url" />
+      <el-table-column label="后台URL" width="150px" align="center" prop="backonUrl" />
+      <el-table-column label="请求体配置" width="150px" align="center" prop="requestBody" />
+      <el-table-column label="最大缓存条目" width="150px" align="center" prop="resultNum" />
+      <el-table-column label="过期时间" width="150px" align="center" prop="expireTime" />
+      <el-table-column label="状态" width="150px" align="center">
+        <template slot-scope="{row}">
+          {{ row.status ? '生效' : '失效' }}
+        </template>
+      </el-table-column>
+      <el-table-column label="创建时间" width="150px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.createTime }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="更新时间" width="150px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.updateTime }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="操作" align="center" width="230" class-name="small-padding fixed-width" fixed="right">
+        <!-- <template slot-scope="{row}"> -->
+        <el-button type="primary" size="mini">编辑</el-button>
+        <el-button type="primary" size="mini">删除</el-button>
+        <el-button type="primary" size="mini">失效</el-button>
+
+        <!-- </template> -->
+      </el-table-column>
+    </el-table>
+
+    <div class="block">
+      <el-pagination
+        align="center"
+        background
+        :current-page="1"
+        :page-sizes="[5, 10, 20, 50]"
+        :page-size="10"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="list.length"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import waves from '@/directive/waves' // waves directive
+import { parseTime } from '@/utils'
+import { getList } from '@/api/cache'
+
+export default {
+  name: 'ComplexTable',
+  directives: { waves },
+  filters: {
+
+  },
+  data() {
+    return {
+      tableKey: 0,
+      list: null,
+      total: 0,
+      listLoading: true,
+      listQuery: {
+        pageNo: 1,
+        pageSize: 10,
+        importance: undefined,
+        title: undefined,
+        type: undefined,
+        sort: '+id'
+      }
+
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+      getList({ 'page': 1, 'size': 10 }).then(response => {
+        this.listLoading = false
+        this.list = response.data.records
+        console.log(response.data.records)
+        console.log(response)
+      })
+    },
+
+    handleSizeChange() {
+
+    },
+    handleCurrentChange() {},
+    handleFilter() {
+      this.listQuery.page = 1
+      this.getList()
+    },
+    handleModifyStatus(row, status) {
+      this.$message({
+        message: '操作Success',
+        type: 'success'
+      })
+      row.status = status
+    },
+    sortChange(data) {
+      const { prop, order } = data
+      if (prop === 'id') {
+        this.sortByID(order)
+      }
+    },
+    sortByID(order) {
+      if (order === 'ascending') {
+        this.listQuery.sort = '+id'
+      } else {
+        this.listQuery.sort = '-id'
+      }
+      this.handleFilter()
+    },
+    resetTemp() {
+      this.temp = {
+        id: undefined,
+        importance: 1,
+        remark: '',
+        timestamp: new Date(),
+        title: '',
+        status: 'published',
+        type: ''
+      }
+    },
+    handleCreate() {
+      this.resetTemp()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    createData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          this.temp.id = parseInt(Math.random() * 100) + 1024 // mock a id
+          this.temp.author = 'vue-element-admin'
+        }
+      })
+    },
+    handleUpdate(row) {
+      this.temp = Object.assign({}, row) // copy obj
+      this.temp.timestamp = new Date(this.temp.timestamp)
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    updateData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          const tempData = Object.assign({}, this.temp)
+          tempData.timestamp = +new Date(tempData.timestamp) // change Thu Nov 30 2017 16:41:05 GMT+0800 (CST) to 1512031311464
+        }
+      })
+    },
+    handleDelete(row) {
+      this.$notify({
+        title: 'Success',
+        message: 'Delete Successfully',
+        type: 'success',
+        duration: 2000
+      })
+      const index = this.list.indexOf(row)
+      this.list.splice(index, 1)
+    },
+    handleFetchPv(pv) {
+
+    },
+    handleDownload() {
+
+    },
+    formatJson(filterVal, jsonData) {
+      return jsonData.map(v => filterVal.map(j => {
+        if (j === 'createTime') {
+          return parseTime(v[j])
+        } else {
+          return v[j]
+        }
+      }))
+    },
+    getSortClass: function(key) {
+      const sort = this.listQuery.sort
+      return sort === `+${key}`
+        ? 'ascending'
+        : sort === `-${key}`
+          ? 'descending'
+          : ''
+    }
+  }
+}
+</script>
