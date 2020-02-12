1a9ca6e7f2bfc3a82e22c9be502b123de848bbc5
HarryJiangter <15756329950@163.com>
2020-02-12 16:56:17 +0800

    修改

--- src/api/serviceArrange.js
+++ src/api/serviceArrange.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 export function addServiceArrange(params) {
   return request({
     baseURL: 'http://localhost:9000/gateway',
-    url: '/serviceArrange/addServiceArrange',
+    url: '/serviceArrange/addServiceArrangeBak',
     method: 'post',
     data: params
   })
